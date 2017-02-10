7e8376f5d2a4f0e64b620d4e75e5b021088eb5ec
0xFireball <0xFireball@outlook.com>
2017-02-10 15:55:25 -0800

    update n4

---
+++ Sources/states/MenuState.hx
@@ -0,0 +1,13 @@
+package states;
+
+import n4.NState;
+import n4e.ui.NEText;
+
+class MenuState extends NState {
+	override public function create() {
+		
+		var titleText = new NEText(20, 20, "SuperBoats", 20);
+
+		super.create();
+	}
+}
