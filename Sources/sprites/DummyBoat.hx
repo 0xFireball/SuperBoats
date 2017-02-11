606b3a327d19d0e40df124b100ed48a04da34a26
0xFireball <0xFireball@outlook.com>
2017-02-10 17:34:25 -0800

    add cool menu

---
+++ Sources/sprites/DummyBoat.hx
@@ -0,0 +1,15 @@
+package sprites;
+
+import kha.Color;
+
+import n4.math.NPoint;
+import n4.NGame;
+import n4.entities.NSprite;
+import n4.effects.particles.NSquareParticleEmitter;
+import n4.NState;
+import n4e.ui.NEText;
+import n4.util.NAxes;
+
+class DummyBoat extends NSprite {
+	
+}
